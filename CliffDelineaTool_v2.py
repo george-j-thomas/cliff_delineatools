6b6a2735c4067a158c105b91617f5ca1a0405dd0
g4thomas <g4thomas@ucsd.edu>
2022-03-11 13:22:21 -0800

    Add fix_line_shp function and cleaned up other functions

--- CliffDelineaTool_v2.py
+++ CliffDelineaTool_v2.py
@@ -1,35 +1,16 @@
-# CliffDelineaToolPy v1.0.0
-# An algorithm to map coastal cliff base and top from topography
-# https://github.com/zswirad/CliffDelineaTool
-# Zuzanna M Swirad (zswirad@ucsd.edu), Scripps Institution of Oceanography, UC San Diego
-# Originally coded in MATLAB 2019a (v1.2.0; 2021-11-24)
-# Help in debugging: George Thomas (2021-12-2)
-# Last updated on 2021-12-3 (Python 3.8)
-
 import os
 import pandas as pd
 import numpy as np
 import math
 import statsmodels.api as sm
 import glob
+import geopandas as gpd
+from shapely.geometry import LineString, Point, MultiLineString
+from shapely.ops import linemerge
 
-# Indicate input data parameters:
-os.chdir(r"C:\Users\g4thomas\Documents\CliffDelineation\Files\2_To_Delineate_txt")
 
-fileName = '20211103_00518_00568_NoWaves_Blacks_CliffPoints.txt'#'20200220_00982_01170_NoWaves_MASS_Camp_Pendleton_CliffPoints.txt'
-top = False
+def get_slopes(data,nVert):
 
-# Set the calibrated input variables:
-nVert = 5#20 # How many adjacent points to consider as a local scale?
-baseMaxElev = 10 # What is the top limit for cliff base elevation (m)?
-baseSea = 20#15; # What is the max seaward slope for cliff base (deg)?
-baseLand = 20#25; # What is the min landward slope for cliff base (deg)? - seems to have larger impact than bS
-topSea = 20 # What is the min seaward slope for cliff top (deg)?
-topLand = 15 # What is the max landward slope for cliff top (deg)?
-propConvex = 0.5 # What is the minimal proportion of the distance from trendline #2 to replace modelled cliff top location?
-smoothWindow = 10 # What is the alongshore moving window for cross-shore smoothing (points)? # changing to 5 did nothing
-
-def get_slopes(data):
     """ Get slopes of transect lines for later delineation.
 
     Takes in original cliff points data and returns table including:
@@ -121,14 +102,46 @@ def get_slopes(data):
     table = table.reset_index(drop=True)
     return table
 
-def model_base(table):
+def fix_line_shp(cliffline):
+    # Merge whole line together, filling gaps along the way
+    cliffline_multi = MultiLineString(list(cliffline['geometry']))
+    cliffline_merge = linemerge(cliffline_multi)
+    # Remove 'bad' cliff linestrings that are only 2 points
+    goodline_list = []
+    for line in cliffline_merge.geoms:
+        if len(line.coords) > 2:
+            goodline_list.append(line)
+    # Fill Gaps in between 'good' cliff linestrings with small straight lines 
+    # (Need continuous linestring for splitting)
+    connectors = []
+    for line_i in range(len(goodline_list)-1):
+        first = Point(goodline_list[line_i].coords[0])
+        last = Point(goodline_list[line_i+1].coords[-1])
+        connectors.append(LineString([first,last]))
+    full_list = list(goodline_list) + list(connectors)
+    full_line = linemerge(full_list)
+    return full_line
+
+def model_base(
+        table,
+        baseMaxElev,
+        baseSea,
+        baseLand,
+        **kwargs
+    ):
     """ Finds potential cliff base locations using input variables
 
     """
     # Find potential cliff base locations:
-    potential_base = table[(table['Elevation'] < baseMaxElev) & (table['SeaSlope'] < baseSea) & (table['LandSlope'] > baseLand) & (table['Difference1'] < 0)]   
+    potential_base = table[
+                        (table['Elevation'] < baseMaxElev) 
+                        & (table['SeaSlope'] < baseSea) 
+                        & (table['LandSlope'] > baseLand) 
+                        & (table['Difference1'] < 0)
+                    ]   
     
-    # From the points that satisfy the criteria, for each transect select one with the largest vertical difference between the elevation and trendline #1:
+    # From the points that satisfy the criteria, for each transect select one with the largest
+    #  vertical difference between the elevation and trendline #1:
     modelled_base = pd.DataFrame() 
     if potential_base.shape[0] > 0:
         cliffed_profiles = potential_base['TransectID'].unique()
@@ -138,9 +151,42 @@ def model_base(table):
                     sub = potential_base[potential_base['TransectID'] == n]
                     sub = sub.sort_values(by=['Difference1'])
                     modelled_base = modelled_base.append(sub.iloc[0])
+    
+    # If there was a previous line for this area, use it to constrain the new line
+    if 'previous_base' in kwargs:
+        previous_base = gpd.read_file(kwargs.get('previous_base'))
+        if len(previous_base) > 1:
+            prev_line = fix_line_shp(previous_base)
+
+        possible_problem_transects = []
+        for tID in modelled_base['TransectID']:
+            base_point = Point(
+                    modelled_base[modelled_base.TransectID == tID].UTM_E.item(),
+                    modelled_base[modelled_base.TransectID == tID].UTM_N.item()
+                    )
+            if prev_line.distance(base_point) > 5:
+                transect = LineString(zip(
+                            table[table.TransectID == tID].UTM_E.values,
+                            table[table.TransectID == tID].UTM_N.values)
+                            )
+                possible_problem_transects.append(transect)
+        prob_transects_gp = gpd.GeoSeries(possible_problem_transects)
+        prob_transects_gp.to_file('transects_off_5mOrGreater.shp')
+            
+            
+
+    
     return modelled_base, cliffed_profiles
 
-def model_top(modelled_base, cliffed_profiles,table):
+def model_top(
+        modelled_base, 
+        cliffed_profiles,
+        table,
+        nVert,
+        topSea,
+        topLand,
+        propConvex
+    ):
     """ Finds potential cliff top locations
 
     """
@@ -180,14 +226,16 @@ def model_top(modelled_base, cliffed_profiles,table):
                 if potential_top.shape[0] > 0:
                     potential_top = potential_top.sort_values(by=['Difference2'])
                     
-                    # From the points that satisfy the criteria, for each transect select one with the largest vertical difference between the elevation and trendline #2:
+                    # From the points that satisfy the criteria, for each transect select one with the 
+                    # largest vertical difference between the elevation and trendline #2:
                     modelled_top0 = potential_top.iloc[-1]   
                     
                     # Check whether the selected point is part of within-cliff flattening:
                     if potential_top['Distance'].max() > modelled_top0.Distance + nVert:
                         subNew = sub.copy()
                         sub_top_dist = potential_top.iloc[-1, sub.columns.get_loc('Distance')]
-                        subNew.drop(subNew[subNew['Distance'] < sub_top_dist].index, inplace = True) # remove points seawards from the modelled cliff top
+                        # remove points seawards from the modelled cliff top
+                        subNew.drop(subNew[subNew['Distance'] < sub_top_dist].index, inplace = True) 
                         rowCountNew = subNew.shape[0]
                         zerosNew = np.zeros(rowCountNew + 1)
                         subNew['Trendline3'] = pd.Series(zerosNew)
@@ -225,7 +273,14 @@ def model_top(modelled_base, cliffed_profiles,table):
                     modelled_top = modelled_top.append(modelled_top0)
     return modelled_top
 
-def fix_top(fix, modelled_top, modelled_base, table):
+def fix_top(
+        fix, 
+        modelled_top, 
+        modelled_base, 
+        table,
+        topSea,
+        topLand
+        ):
     """ Re-models cliff top locations, removing outliers
     
     """
@@ -237,7 +292,7 @@ def fix_top(fix, modelled_top, modelled_base, table):
         outlier = modelled_top[modelled_top['TransectID'] == fix.TransectID[c]]
         
         # Remove points seawards from the cliff base:
-        sub_base = modelled_base[modelled_base['TransectID'] == c] # THIS ORIGINALLY HAD "== n" which I don't think makes sense   
+        sub_base = modelled_base[modelled_base['TransectID'] == fix.TransectID[c]] # THIS ORIGINALLY HAD "== n", corrected 1/24/21 to current   
         sub_base = sub_base.reset_index(drop=True)
         sub_base_dist = sub_base.Distance[0]
         sub.drop(sub[sub['Distance'] < sub_base_dist].index, inplace = True)
@@ -297,35 +352,74 @@ def fix_top(fix, modelled_top, modelled_base, table):
     modelled_top.drop(modelled_top[modelled_top['Outlier'] == 1].index, inplace = True) # ignore new cliff top positions if standardized residuals did not improve
     return modelled_top
     
-def delineate_cliff(fileName,top,nVert=20,baseMaxElev=10,baseSea=20,baseLand=20,topSea=20,topLand=15,propConvex=0.5,smoothWindow=10):
-    with open(fileName, 'r') as fin:
+def delineate_cliff(
+        cliff_points_path,
+        top,
+        nVert=20,
+        baseMaxElev=10,
+        baseSea=20,
+        baseLand=20,
+        topSea=20,
+        topLand=15,
+        propConvex=0.5,
+        smoothWindow=10,
+        **kwargs
+    ):
+
+    """ Finds cliff base (and top) for a given DEM using points created by .
+
+    Basic Arguments:
+    cliff_points_path -- path to cliff points csv created by points_from_dem.py module
+    top -- boolean flag of whether to compute cliff top (vs just base)
+
+    Calibrated Input Arguments:
+    nVert -- How many adjacent points to consider as a local scale?
+    baseMaxElev -- What is the top limit for cliff base elevation (m)?
+    baseSea -- What is the max seaward slope for cliff base (deg)?
+    baseLand -- What is the min landward slope for cliff base (deg)? - seems to have larger impact than bS
+    topSea -- What is the min seaward slope for cliff top (deg)?
+    topLand -- What is the max landward slope for cliff top (deg)?
+    propConvex -- What is the minimal proportion of the distance from trendline #2 to replace modelled cliff top location?
+    smoothWindow -- What is the alongshore moving window for cross-shore smoothing (points)?
+
+    See below for more information:
+    # An algorithm to map coastal cliff base and top from topography
+    # https://github.com/zswirad/CliffDelineaTool
+    # Zuzanna M Swirad (zswirad@ucsd.edu), Scripps Institution of Oceanography, UC San Diego
+    # Adapted for this module by George Thomas
+
+    """
+    
+    fileName = os.path.basename(cliff_points_path)
+    folder = os.path.split(cliff_points_path)[0]
+
+    with open(cliff_points_path, 'r') as fin:
         data = fin.read().splitlines(True)
-    with open(fileName, 'w') as fout:
+    with open(cliff_points_path, 'w') as fout:
         fout.writelines(data[1:])
 
-    data = pd.read_csv(fileName, header = None)
+    data = pd.read_csv(cliff_points_path, header = None)
     data.columns =['PointID', 'TransectID', 'Elevation', 'Distance','UTM_E','UTM_N'] # Give column names; you can add XY coordinates etc if they were exported from GIS software
     data = data[data.Elevation != -9999]
 
-    table = pd.DataFrame()
-   
-    table = get_slopes(data)
-            
-    modelled_base, cliffed_profiles = model_base(table)
+    # Get slopes data from get_slopes function
+    table = get_slopes(data,nVert)
+    
+    # Use slopes data to find modelled cliff base
+    modelled_base, cliffed_profiles = model_base(table,baseMaxElev,baseSea,baseLand,**kwargs)
      
     # Find cliff top locations for transects with cliff base:
     if modelled_base.shape[0] > 0:    
 
         # Save the data:
-        saveName1 = fileName[:9]+fileName[29:-16] + '_nVert{}_bME{}_bS{}_bL{}_tS{}_tL{}_pC{}_sW{}_base.txt'.format(nVert,baseMaxElev,baseSea,baseLand,topSea,topLand,propConvex,smoothWindow)
+        saveName1 = fileName[:-4] + '_nVert{}_bME{}_bS{}_bL{}_tS{}_tL{}_pC{}_sW{}_base.txt'.format(nVert,baseMaxElev,baseSea,baseLand,topSea,topLand,propConvex,smoothWindow)
 
-        folder = r"C:\Users\g4thomas\Documents\CliffDelineation\Files\3_Delineated_csv"
         outpath = os.path.join(folder,saveName1)
         modelled_base_save = modelled_base[['PointID', 'TransectID','UTM_E','UTM_N']] # Select which columns to save; you may want to add XY coordinates if they were present
         modelled_base_save.to_csv(outpath, header=False, index = False) # change to header=True if exporting with header     
 
         if top:
-            modelled_top = model_top(modelled_base, cliffed_profiles,table)
+            modelled_top = model_top(modelled_base,cliffed_profiles,table,nVert,topSea,topLand,propConvex)
                             
             # Remove alongshore outliers:
             # 1. Find outliers:   
@@ -355,30 +449,42 @@ def delineate_cliff(fileName,top,nVert=20,baseMaxElev=10,baseSea=20,baseLand=20,
             # 2. Delete or replace outliers with more suitable potential cliff tops:
             # (Repeat cliff top detection for the transects with outliers.)
             if fix.shape[0] > 0:
-                modelled_top = fix_top(fix, modelled_top, modelled_base, table)
+                modelled_top = fix_top(fix,modelled_top,modelled_base,table,topSea,topLand)
 
             if modelled_top.shape[0] > 0:
-                saveName2 = fileName[:-4] + '_top.txt'
+                saveName2 = fileName[:-4] + '_nVert{}_bME{}_bS{}_bL{}_tS{}_tL{}_pC{}_sW{}_top.txt'.format(nVert,baseMaxElev,baseSea,baseLand,topSea,topLand,propConvex,smoothWindow)
                 modelled_top_save = modelled_top[['PointID', 'TransectID','UTM_E','UTM_N']] # Select which columns to save; you may want to add XY coordinates if they were present
                 outpath = os.path.join(folder,saveName2)
                 modelled_top_save.to_csv(outpath, header=False, index = False) # change to header=True if exporting with header    
 
-def average_cliffline(fileName,top=False):
-    import geopandas as gpd
-    from shapely.geometry import LineString 
-    folder = r"C:\Users\g4thomas\Documents\CliffDelineation\Files\3_Delineated_csv"
+def nVert_test(cliff_points_path,top):
+    n_values = np.linspace(5,25,5)
+    for nVert in n_values:
+        delineate_cliff(cliff_points_path,top,nVert)
+    return
+
+def average_cliffline(cliff_points_path,top=False):
+
+    nVert_test(cliff_points_path,top)
+    
+    fileName = os.path.basename(cliff_points_path)
+    folder = os.path.split(cliff_points_path)[0]    
     fulldata = pd.DataFrame(columns =['PointID', 'TransectID','UTM_E','UTM_N'])
+    
     search = os.path.join(folder,fileName[:9]+fileName[29:-16]+r'*base.txt')
+
     for infile in glob.glob(search):
         data = pd.read_csv(infile, header = None)
         data.columns =['PointID', 'TransectID','UTM_E','UTM_N'] 
         fulldata = fulldata.append(data,ignore_index=True)
+
     transects = sorted(fulldata.TransectID.unique())
     utm_e = []
     utm_n = []
     for tID in transects:
         utm_e.append(np.mean(fulldata['UTM_E'].loc[fulldata['TransectID'] == tID]))
         utm_n.append(np.mean(fulldata['UTM_N'].loc[fulldata['TransectID'] == tID]))
+        
     coordinates = list(zip(utm_e,utm_n))
     line = LineString(coordinates)
     gdr = gpd.GeoSeries(line,crs='epsg:26911')
@@ -406,8 +512,3 @@ def average_cliffline(fileName,top=False):
         gdr.to_file(outpath)
 
     return
-
-def nVert_test(fileName,top):
-    n_values = np.linspace(5,25,5)
-    for nVert in n_values:
-        delineate_cliff(fileName,top,nVert)
